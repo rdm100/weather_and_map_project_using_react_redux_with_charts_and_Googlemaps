56cf2b8e9cf1ec9b05c87527d0127bfc0436cc5e
rdm100 <robertode_marco@hotmail.com>
2018-09-18 18:28:12 +0100

    Added pressure and humidity charts for each city too.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,13 +6,15 @@ class WeatherList extends Component {
 	renderWeather() {
 			return this.props.weather.map((cityData) => {
 			const temps = cityData.list.map((weather) => weather.main.temp);
+			const pressures = cityData.list.map((weather) => weather.main.pressure);
+			const humidities = cityData.list.map((weather) => weather.main.humidity);
 			console.log(temps);
 			return (
 			<tr key={cityData.city.name}>
 				<td>{cityData.city.name}</td>
-				<td>
-				<Chart data={temps} color="orange" />
-				</td>
+				<td><Chart data={temps} color="orange" /></td>				
+				<td><Chart data={pressures} color="blue" /></td>
+				<td><Chart data={humidities} color="green" /></td>
 			</tr>
 				)
 		});
